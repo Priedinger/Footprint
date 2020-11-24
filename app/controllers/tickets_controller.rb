fdc49e354e58c51bc0491cfad202d719e98f780d
pierre riedinger <riedingerpierre@gmail.com>
2020-11-24 16:00:12 +0100

    vous avez tout vu

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -7,10 +7,25 @@ class TicketsController < ApplicationController
   def show
     @ticket = Ticket.find(params[:id])
   end
-  
+
   def new
     @ticket = Ticket.new
   end
+
+  def create
+    # string séparées par des virgules ex: "papier toilette, hule d'olive, déodorant"
+    @ticket = Ticket.new(tickets_params)
+    @ticket.user = current_user
+    @ticket.save
+    items = params[:ticket][:photo]
+    items.split(',').each do |item|
+      # vérifier si l'item existe déjàs
+      new_item = Item.create!(description: item)
+      TicketLine.create(ticket_id: @ticket.id, item_id: new_item.id, quantity: 1)
+    end
+    redirect_to ticket_items_path(@ticket)
+  end
+
 
   def destroy
     @ticket = Ticket.find(params[:id])
@@ -20,7 +35,7 @@ class TicketsController < ApplicationController
 
 private
 
-  def items_params
+  def tickets_params
     params.require(:ticket).permit(:photo, :user_id)
   end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,5 @@
 class Ticket < ApplicationRecord
   belongs_to :user
   has_many :ticket_lines
+  has_many :items, through: :ticket_lines
 end
