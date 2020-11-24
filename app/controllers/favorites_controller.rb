e9f419dbf9ff9aafc310bce66cd57c950ae02bb2
Vincent Huche <vincent@huche.net>
2020-11-24 13:11:39 +0100

    create favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,4 +2,23 @@ class FavoritesController < ApplicationController
   def index
     @favorites = current_user.favorites
   end
+
+  def create
+    @favorite = Favorite.new(favorite_params)
+    @favorite.user = current_user
+    @product = Product.find(params[:product_id])
+    @favorite.product = @product
+    if @favorite.save
+      redirect_to product_path(@product), notice: "Ajouté aux favoris"
+    else
+      render 'products/show'
+    end
+  end
+
+  private
+
+  def favorite_params
+    params.require(:favorite).permit(:user_id, :product_id)
+  end
+
 end
