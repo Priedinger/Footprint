faf67507a4c57afd607baf3a900791b55da51006
pierre riedinger <riedingerpierre@gmail.com>
2020-11-27 13:38:35 +0100

    photo obligatoire pour créer un ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -19,21 +19,24 @@ class TicketsController < ApplicationController
     # string séparées par des virgules ex: "papier toilette, hule d'olive, déodorant"
     @ticket = Ticket.new(tickets_params)
     @ticket.user = current_user
-    @ticket.save
-    items = params[:ticket][:photo]
-    items.split(',').each do |item|
-      # vérifier si l'item existe déjà
-      if Item.where(description: item).where.not(product_id: nil).first
-        # si cette description(item) est déjà associé à un produit dans la DB, on récupère l'item et on fait une ticket line avec
-        item = Item.where(description: item).where.not(product_id: nil)
-        TicketLine.create!(ticket_id: @ticket.id, item_id: item.first.id, quantity: 1)
-      else
-        # autrement on crée un nouvel item et une ticket line
-        new_item = Item.create!(description: item)
-        TicketLine.create(ticket_id: @ticket.id, item_id: new_item.id, quantity: 1)
+    if @ticket.save
+      items = params[:ticket][:photo]
+      items.split(',').each do |item|
+        # vérifier si l'item existe déjà
+        if Item.where(description: item).where.not(product_id: nil).first
+          # si cette description(item) est déjà associé à un produit dans la DB, on récupère l'item et on fait une ticket line avec
+          item = Item.where(description: item).where.not(product_id: nil)
+          TicketLine.create!(ticket_id: @ticket.id, item_id: item.first.id, quantity: 1)
+        else
+          # autrement on crée un nouvel item et une ticket line
+          new_item = Item.create!(description: item)
+          TicketLine.create(ticket_id: @ticket.id, item_id: new_item.id, quantity: 1)
+        end
       end
+        redirect_to ticket_path(@ticket)
+    else
+      render :new
     end
-    redirect_to ticket_path(@ticket)
   end
 
   def destroy
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,5 @@ class Ticket < ApplicationRecord
   belongs_to :user
   has_many :ticket_lines, dependent: :destroy
   has_many :items, through: :ticket_lines
+  validates :photo, presence: true
 end
