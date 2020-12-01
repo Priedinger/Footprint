866e042a73cbf700f3fd225045ab0d9ce9a27d04
Vincent Huche <vincent@huche.net>
2020-12-01 12:16:41 +0100

    score dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,35 @@ class PagesController < ApplicationController
     if current_user
       @favorites = current_user.favorites
       @tickets = current_user.tickets
+      global_score
     end
   end
 
   def settings
     @title = "Settings"
+  end
+
+  private
+
+  def global_score
+    @tickets = current_user.tickets
+    @global_tickets = @tickets.map do |ticket|
+      calculate_ticket_score(ticket)
+    end
+    @global_tickets_score = @global_tickets.reduce(0, :+) / @tickets.size
+  end
+
+  def calculate_ticket_score(ticket)
+    total_score = 0
+    ticket.items.where.not(product_id: nil).each do |item|
+      total_score += item.product.score
+    end
+    ticket_items_count = ticket.items.where.not(product_id: nil).count
+    if ticket_items_count.zero?
+      ticket_score = 0
+    else
+      ticket_score = total_score / ticket.items.where.not(product_id: nil).count
+    end
   end
 
 end
