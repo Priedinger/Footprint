c2faf751cf0313676602127ad247c98f0345fee0
Victor Branger <victor.branger@gmail.com>
2020-12-02 16:11:03 +0100

    light color in progress circle

--- app/javascript/components/progress-circle.js
+++ app/javascript/components/progress-circle.js
@@ -8,12 +8,15 @@ const initProgressBar = () => {
     if (valeur < 33) {
       barre.style.borderColor = "#FF5154";
       barre50.style.borderColor = "#FF5154";
+      circle.style.borderColor = "#FFE7E7";
     } else if (valeur < 66) {
       barre.style.borderColor = "#E67E22";
       barre50.style.borderColor = "#E67E22";
+      circle.style.borderColor = "#FFF4D3";
     } else {
       barre.style.borderColor = "#0AAF90";
       barre50.style.borderColor = "#0AAF90";
+      circle.style.borderColor = "#E2FFEE";
     }
   }
 }
