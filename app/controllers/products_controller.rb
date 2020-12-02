8bcd07ebdd791301a52c905373616f9db9d3fbd5
Elsa Lebas <lebas.elsa@gmail.com>
2020-12-02 16:48:29 +0100

    fixed bug with regex

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,12 @@
 class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
-    @title = @product.name
+    if @product.name.match?(/&quot;/)
+      #sentence.gsub! 'Robert', 'Joe'
+      @title = @product.name.gsub! '&quot;', '"'
+    else
+      @title = @product.name
+    end
     # création array alternatives : les produits ayant la même category agribalyse en excluant le produit de la show actuelle
     @alternatives = Product.where(category_agribalyse: @product.category_agribalyse).reject{|product| product.id == @product.id}
   end
