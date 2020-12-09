38fb9ccf70020373967026ffc814455a0fbf4bcd
Victor Branger <victor.branger@gmail.com>
2020-12-09 15:48:46 +0100

    clean sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   include Pundit
 
   # Pundit: white-list approach.
@@ -16,6 +18,11 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { host: ENV["https://www.foot-print.xyz/"] || "localhost:3000" }
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
 
   # private
 
